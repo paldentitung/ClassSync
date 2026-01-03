ad3f127d11474c02b8f60b71dfff99791c7b91d4
Palden D. Titung <paldendorjetitung@gmail.com>
2026-01-03 17:41:02 +0545

    added the post method for assignments in backend and check with postman

--- backend/controllers/assignmentsContoller.js
+++ backend/controllers/assignmentsContoller.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const file = require("path");
+const { json } = require("stream/consumers");
 
 const filePath = file.join(__dirname, "../data/assignmentsData.json");
 
@@ -7,7 +8,41 @@ const getAssignment = () => {
   const data = fs.readFileSync(filePath, "utf8");
   return JSON.parse(data);
 };
+
+const saveAssignments = (assignment) => {
+  fs.writeFileSync(filePath, JSON.stringify(assignment, null, 2));
+};
 exports.getAllAssignment = (req, res) => {
   const data = getAssignment();
   return res.json(data);
+};
+
+exports.createAssignment = (req, res) => {
+  const { title, subject, dueDate, assignmentStatus } = req.body;
+  if (!title || !subject || !dueDate || !assignmentStatus) {
+    res.status(400).json({
+      message: "Invalid data",
+    });
+  }
+
+  const data = getAssignment();
+
+  const newAssignment = {
+    id: Date.now(),
+    title,
+    subject,
+    dueDate,
+    assignmentStatus,
+  };
+
+  data.push(newAssignment);
+
+  // update the file after adding new assignment
+
+  saveAssignments(data);
+
+  res.status(201).json({
+    message: "Assignment created",
+    assignment: newAssignment,
+  });
 };
--- backend/routes/assignmentsRoute.js
+++ backend/routes/assignmentsRoute.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const { getAllAssignment } = require("../controllers/assignmentsContoller");
+const {
+  getAllAssignment,
+  createAssignment,
+} = require("../controllers/assignmentsContoller");
 const router = express.Router();
 
 router.get("/", getAllAssignment);
+router.post("/", createAssignment);
 module.exports = router;
